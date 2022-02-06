b4129d9c9c078d794466d899de800e1180c46f72
villiamriegler <74962511+villiamriegler@users.noreply.github.com>
2022-02-06 11:10:41 +0100

    Add files via upload

--- chessEngine.py
+++ chessEngine.py
@@ -4,6 +4,7 @@ from shutil import move
 
 class gameState (): 
     def __init__(self):
+        #Setting up the board with all pieces
         self.board = [
             ["bR","bN","bB","bQ","bK","bB","bN","bR"],
             ["bP","bP","bP","bP","bP","bP","bP","bP"],
@@ -14,17 +15,37 @@ class gameState ():
             ["wP","wP","wP","wP","wP","wP","wP","wP"],
             ["wR","wN","wB","wQ","wK","wB","wN","wR"]
         ]
+        #Keping track of whose tur it is and the moves made 
         self.whiteMove = True
         self.moveLog = []
 
+        #Keeping tack of kings for checks, pins, stalemates and, checkmates
+        self.wKLoc = (7,4) #Starting positions
+        self.bKLoc = (0,4)
+
+    """
+    Function for moving a piece from one location to the next [doesn't check if move is valid at all]
+        Args: Move object containing starting pos and ending pos and the pieces already on those squares
+    """
     def makeMove (self,move): 
+        #emptys the starting position of the moving piece 
         self.board[move.startR][move.startC] = "--"
+        
+        #places the moving piece on the new square 
         self.board[move.endR][move.endC] = move.piceMoved 
+        
+        #appends to the movelog and switches turn
         self.moveLog.append(move)
         self.whiteMove = not self.whiteMove
 
+    """
+    Function for unding the last move
+        uses the self.moveLog to replace a captured piece and place back the caputing piece 
+    """
     def undoMove(self):
+        #Checking so that a move has actually been made 
         if len(self.moveLog) > 0:
+            #same as move function only uses a previous move object
             lastMove = self.moveLog.pop()
             self.board[lastMove.startR][lastMove.startC] = lastMove.piceMoved 
             self.board[lastMove.endR][lastMove.endC] = lastMove.caputerdPiece
@@ -33,13 +54,20 @@ class gameState ():
     def getValidMoves(self):
         return self.getAllMoves()
 
+    """
+    Gets all valid moves acording to how the pieces are alowed to move [ignores checks, pins and so on]
+    """
     def getAllMoves(self):
-        moves = [] 
+        moves = [] #stores all valid moves 
+        
+        #loops thru all rows and colums of the board 
         for r in range(len(self.board)):
             for c in range(len(self.board[r])):
-                turn = self.board[r][c][0] #kollar på första bokstaven av en given pjäs t.ex. bP => b alltså svart pjäs eller -- => - inte en pjäs
+                turn = self.board[r][c][0] #checks the color of the moving piece 
                 if (turn == "w" and self.whiteMove) or (turn == "b" and not self.whiteMove):
-                    piece = self.board[r][c][1] #kollar på andra bokstaven t.ex. om bP => P allstå en bonde
+                    piece = self.board[r][c][1] #Checks the kind of piece 
+                    
+                    #running the right function for the right piece type 
                     if (piece == "P"): 
                         self.getPawnMoves(r,c,moves)
                     elif (piece == "R"):
@@ -55,21 +83,23 @@ class gameState ():
         return moves
 
     """
-    Get moves for piece located at (r,c) and add to list om possible moves
+    Get moves for piece located at (r,c) and add to list of possible moves
     """
     def getPawnMoves(self,r,c,moves):
         if (self.whiteMove): #White pawn moves 
-            if (self.board[r-1][c] == "--"):
+            if (self.board[r-1][c] == "--"): #Cheking if the square infront is empty and if the pawn is in starting pos if the two squares infron are empty
                 moves.append(Move((r,c),((r-1),c),self.board))
-
                 if (r == 6) and (self.board[r-2][c]=="--"):
                     moves.append(Move((r,c),((r-2),c),self.board))
+            
+            #Checks diagonals for captures
             if (c-1 >= 0): 
                 if (self.board[r-1][c-1][0] == "b"):
                     moves.append(Move((r,c),((r-1),(c-1)),self.board))
             if(c+1 <= 7):
                 if (self.board[r-1][c+1][0] == "b"):
                     moves.append(Move((r,c),((r-1),(c+1)),self.board))
+        
         else: #black pawn moves
             if (self.board[r+1][c]=="--"):
                 moves.append(Move((r,c),(r+1,c),self.board))
@@ -83,77 +113,91 @@ class gameState ():
                     moves.append(Move((r,c),(r+1,c+1),self.board))
 
     def getRookMoves(self,r,c,moves,dir=((1,0),(0,1),(-1,0),(0,-1))):
+        #defining the oposposing teams color
         other = "b" if self.whiteMove else "w"
 
+        #looping thru the directions to check, for a roock we check up,down,left,right 
         for d in dir: 
-            for i in range(1,8):
-                endR = r+d[0]*i
+            for i in range(1,8): #Loping thru the lenght of the board 
+                endR = r+d[0]*i #takes the starting pos and loops thur each next square in given direction 
                 endC = c+d[1]*i
-                if (0 <= endR < 8) and (0 <= endC < 8):
+                if (0 <= endR < 8) and (0 <= endC < 8): #If you hit the end of the board 
                     p__ = self.board[endR][endC]
-                    if (p__ == "--"):
+                    if (p__ == "--"): #If there is nothing in the way -> add that move 
                         moves.append(Move((r,c),(endR,endC),self.board))
-                    elif (p__[0] == other):
+                    elif (p__[0] == other): #If there is an enemy in the way add that move and break the loop 
                         moves.append(Move((r,c),(endR,endC),self.board))
                         break
-                    else:
+                    else: #if there is a friendly in the way break the loop
                         break
                 else:
                     break
 
     def getBishopMoves(self,r,c,moves):
+        #A bishop works as a rook moving in different directions, we check all diagonals
         dir = ((1,1),(-1,-1),(1,-1),(-1,1))
         self.getRookMoves(r,c,moves,dir)
 
     def getQueenMoves(self,r,c,moves):
-        dir = ((1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1))
-        self.getRookMoves(r,c,moves,dir)
+        #Works as a combination of a rook and a bishop
+        self.getRookMoves(r,c,moves)
+        self.getBishopMoves(r,c,moves)
    
     def getNightMoves(self,r,c,moves):
-        dir = ((1,2),(2,1),(-1,2),(-2,1),(1,-2),(2,-1),(-1,-2),(-2,-1))
-        same = "w" if self.whiteMove else "b"
+        dir = ((1,2),(2,1),(-1,2),(-2,1),(1,-2),(2,-1),(-1,-2),(-2,-1)) #list of all different jumps a night can do relative to its current pos 
+        same = "w" if self.whiteMove else "b" #defining friendly pieces 
 
-        for d in dir: 
-            endR = r + d[0]
+        for d in dir: #loping thru jumps
+            endR = r + d[0] #finding the end point of that jump
             endC = c + d[1]
-            if 0 <= endR < 8 and 0 <= endC < 8:
-                p__ = self.board[endR][endC]
-                if (p__ != same):
+            if 0 <= endR < 8 and 0 <= endC < 8: #checking so we don't jump out of the playing field
+                p__ = self.board[endR][endC] #looking whats on the landing square 
+                if (p__[0] != same): #If its not a friendy add the move
                     moves.append(Move((r,c),(endR,endC),self.board))
 
     def getKingMoves(self,r,c,moves):
-        dir = ((1,1),(-1,-1),(1,0),(0,1),(-1,0),(0,-1),(-1,1),(1,-1))
-        same = "w" if self.whiteMove else "b"
+        dir = ((1,1),(-1,-1),(1,0),(0,1),(-1,0),(0,-1),(-1,1),(1,-1)) #All relative steps a king can take 
+        same = "w" if self.whiteMove else "b" #Defing same colored pieces 
 
         for i in range(8):
-            endR = r+dir[i][0]
+            endR = r+dir[i][0]#Getting end position of a step
             endC = c+dir[i][1]
-            if 0 <= endR < 8 and 0 <= endC < 8:
+            if (0 <= endR < 8 and 0 <= endC < 8): #keeping within the board 
                 __p = self.board[endR][endC]
-                if (__p[0] != same):
+                if (__p[0] != same): #if not friendly, add the move 
                     moves.append(Move((r,c),(endR,endC),self.board))
 
 
 class Move(): 
+    #converting rows and columns to chess notation 
     rankstoRows = {"1":7,"2":6,"3":5,"4":4,"5":3,"6":2,"7":1,"8":0}
     rowstoRanks = {v: k for k, v in rankstoRows.items()}
     filestoCol = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7}
     colstoFiles = {v:k for k, v in filestoCol.items()}
 
     def __init__(self,sSq,eSq,board):
+        #start and end pos of the move
         self.startR = sSq[0]
         self.startC = sSq[1]
         self.endR = eSq[0]
         self.endC = eSq[1]        
         
+        #effected pieces by the move
         self.piceMoved = board[self.startR][self.startC]
         self.caputerdPiece = board[self.endR][self.endC]
+        
+        #Id for the move (maybe useful for recreating old positions in the future)
         self.moveID = self.startR * 1000 + self.startC * 100 + self.endR * 10 + self.endC
 
     def __eq__(self, __o):
+        #Overwriting the equals sign
         if isinstance(__o, Move):
             return self.moveID == __o.moveID
         return False
+    
+    """
+    Converting a move to chess notation
+    """
     def getChessNot(self):
         return self.getranktoFile(self.startR,self.startC)+ self.getranktoFile(self.endR,self.endC)
 
--- chessMain.py
+++ chessMain.py
@@ -1,3 +1,4 @@
+from math import fabs
 import pygame 
 from chessEngine import *
 
@@ -13,7 +14,7 @@ images = {}
 
 def loadImgs(): 
     """
-    Laddar in bilderna för alla pjäser och sätter in dem i ett dict 
+    loads all images and saves them in a dictionary 
     """
     pieces = ["wR","wN","wB","wQ","wK","wB","wN","wR","bR","bN","bB","bQ","bK","bB","bN","bR","wP","bP"]
 
@@ -21,27 +22,25 @@ def loadImgs():
         images[p] = pygame.transform.scale(pygame.image.load("images/"+p+".png"),(sqSize,sqSize))
 
 
-
-
+"""
+Ritar all grafik för den nuvarande positionen på skärmen
+"""
 def drawGs(screen,gs,sqSelected):
-    """
-    Ritar all grafik för den nuvarande positionen på skärmen
-    """
     drawSq(screen,sqSelected,gs)
     drawPieces(screen, gs.board)
 
 def drawSq(screen,sqSelected,gs):
     colors = [pygame.Color("gray"),pygame.Color("dark gray")]
-    for r in range(dim):
+    for r in range(dim): #loop for drawing the background and highligting pressed pieces
         for c in range(dim):
             color = (r+c)%2
             if sqSelected == (r,c):
-                pygame.draw.rect(screen, pygame.Color("red"), pygame.Rect(c*sqSize,r*sqSize,sqSize,sqSize))  
+                pygame.draw.rect(screen, pygame.Color("red"), pygame.Rect(c*sqSize,r*sqSize,sqSize,sqSize)) 
             else: 
                 pygame.draw.rect(screen, colors[color], pygame.Rect(c*sqSize,r*sqSize,sqSize,sqSize))
 
 def drawPieces(screen,board):
-    for r in range(dim):
+    for r in range(dim): #Draws all the pieces on the board
         for c in range(dim):
             piece = board[r][c]
             if piece != "--": 
@@ -57,6 +56,7 @@ def main():
     clock = pygame.time.Clock()
     screen.fill(pygame.Color("white"))
     
+    #gets the gamestate and checks for valid moves in the starting pos
     gs = gameState()
     validMoves = gs.getValidMoves()
     moveMade = False
@@ -65,10 +65,10 @@ def main():
     #variabler till spelloopen
     loadImgs() #Gör bara en gång 
     run = True
-    sqSelected = ()
+    sqSelected = () #Piece about to be moved 
     playerClicks = [] 
     while run: 
-        for e in pygame.event.get():
+        for e in pygame.event.get(): #loop for getting event handelers
             #Kryssa ner spelet
             if e.type == pygame.QUIT: 
                 run = False
@@ -79,28 +79,29 @@ def main():
                     moveMade = True
             #Flyttar en pjäs        
             elif e.type == pygame.MOUSEBUTTONDOWN:
-                mouseLocation = pygame.mouse.get_pos()
+                mouseLocation = pygame.mouse.get_pos() #gets mouse location on screen and converts to rows and columns
                 c = mouseLocation[0]//sqSize
                 r = mouseLocation[1]//sqSize
-                if sqSelected == (r,c):
+                if sqSelected == (r,c): #If the same square clicked twice cancel the move 
                     sqSelected = ()
                     playerClicks = []
-                else: 
+                else: #Appending the second click 
                     sqSelected = (r,c)
                     playerClicks.append(sqSelected)
-                if len(playerClicks) == 2: 
-                    move = Move(playerClicks[0], playerClicks[1], gs.board)
-                    if move in validMoves:
+                if len(playerClicks) == 2: #if there are two clicks in player clicks, do a move 
+                    move = Move(playerClicks[0], playerClicks[1], gs.board) #definging the move
+                    if move in validMoves: #checks if the move is vaild 
                         gs.makeMove(move)
                         moveMade = True
-                    sqSelected = ()
+                    sqSelected = () #emptys the click variables 
                     playerClicks = []
         
         
-        if moveMade:
+        if moveMade: #if a valid move was made get all new moves
             validMoves = gs.getValidMoves()  
             moveMade = False  
 
+        #Draws the screen
         drawGs(screen,gs,sqSelected)
         clock.tick(maxFps)
         pygame.display.flip()
